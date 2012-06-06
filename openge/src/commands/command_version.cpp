9d78fad7cb31415bdfc1ab0bebc84496e206e679
Lee Baker <lee@leecbaker.com>
2012-06-06 18:06:27 +0700

    Update version number since we have released.

--- openge/src/commands/command_version.cpp
+++ openge/src/commands/command_version.cpp
@@ -23,7 +23,7 @@ namespace po = boost::program_options;
 
 int VersionCommand::runCommand()
 {
-    cerr << "Open Genomics Engine 0.1 (devel)" << endl;
+    cerr << "Open Genomics Engine 0.2 (devel)" << endl;
     cerr << "Built on " __DATE__ " at " __TIME__ << endl;
     cerr << "(c) 2012 Virginia Bioinformatics Institute" << endl;
     
