0af240998de87e91ab6b3d2370afcc7ca757456b
Lee Baker <lee@leecbaker.com>
2012-07-06 15:21:04 +0700

    Modify FASTQ format writer to write separate files for forward, reverse, and orphaned reads.

--- openge/src/util/fastq_writer.cpp
+++ openge/src/util/fastq_writer.cpp
@@ -23,7 +23,9 @@ using namespace std;
 using namespace BamTools;
 
 FastqWriter::FastqWriter() 
-: output_stream(&cout)
+: fwd_stream(&cout)
+, rev_stream(&cout)
+, orphan_stream(&cout)
 , open(false)
 {
     
@@ -33,13 +35,29 @@ bool FastqWriter::Open(const string& filename, const std::string& samHeaderText,
     this->filename = filename;
     
     if(filename != "stdout") {
-        file.open(filename.c_str(), ios::out);
+        fwd_file.open((filename + ".fwd.fastq").c_str(), ios::out);
         
-        if(file.fail()) {
-            cerr << "Failed to open FASTQ output file " << filename << endl;
+        if(fwd_file.fail()) {
+            cerr << "Failed to open FASTQ forward output file " << filename << endl;
             return false;
         }
-        output_stream = &file;
+        fwd_stream = &fwd_file;
+        
+        rev_file.open((filename + ".rev.fastq").c_str(), ios::out);
+        
+        if(rev_file.fail()) {
+            cerr << "Failed to open FASTQ reverse output file " << filename << endl;
+            return false;
+        }
+        rev_stream = &rev_file;
+        
+        orphan_file.open((filename + ".orphan.fastq").c_str(), ios::out);
+        
+        if(orphan_file.fail()) {
+            cerr << "Failed to open FASTQ orphan output file " << filename << endl;
+            return false;
+        }
+        orphan_stream = &orphan_file;
     }
 
     open = true;
@@ -48,23 +66,76 @@ bool FastqWriter::Open(const string& filename, const std::string& samHeaderText,
 }
 
 bool FastqWriter::Close() {
-    if(file.is_open())
-        file.close();
     
+    for(map<string, fastq_record_t>::iterator i = potential_pairs.begin(); i != potential_pairs.end(); i++)
+        *orphan_stream << "@" << i->first << endl << i->second.seq << endl << "+" << i->first << endl << i->second.qual << endl;
+
+    if(open) {
+        fwd_file.close();
+        rev_file.close();
+        orphan_file.close();
+    }
+
     open = false;
     
     return true;
 }
 
+void compliment(string & str)
+{
+    for(int ctr = 0; ctr < str.size(); ctr++) {
+        char & c = str[ctr];
+        switch(c) {
+            case 'A': c = 'T'; break;
+            case 'C': c = 'G'; break;
+            case 'G': c = 'C'; break;
+            case 'T': c = 'A'; break;
+            case 'a': c = 't'; break;
+            case 'c': c = 'g'; break;
+            case 'g': c = 'c'; break;
+            case 't': c = 'a'; break;
+        }
+    }
+}
+
 bool FastqWriter::SaveAlignment(BamTools::BamAlignment & a) {
-    assert(open);
-    
     a.BuildCharData();
     
-    *output_stream << "@" << a.Name << endl;
-    *output_stream << a.QueryBases << endl;
-    *output_stream << "+" << a.Name << endl;
-    *output_stream << a.Qualities << endl;
+    if(fwd_stream == rev_stream)
+        *fwd_stream << "@" << a.Name << endl << a.QueryBases << endl << "+" << a.Name << endl << a.Qualities << endl;
+    else {
+        map<string, fastq_record_t>::iterator it = potential_pairs.find(a.Name);
+
+        if(it == potential_pairs.end()) {
+            fastq_record_t rec;
+            rec.seq = a.QueryBases;
+            rec.qual = a.Qualities;
+            potential_pairs[a.Name] = rec;
+        } else {
+            fastq_record_t & rec = it->second;
+            string seq = a.QueryBases;
+            string qual = a.Qualities;
+
+            string & fwd_seq = a.IsReverseStrand() ? rec.seq : seq;
+            string & fwd_qual = a.IsReverseStrand() ? rec.qual : qual; 
+            string & rev_seq = a.IsReverseStrand() ? seq : rec.seq;
+            string & rev_qual = a.IsReverseStrand() ? qual : rec.qual; 
+
+            reverse(rev_seq.begin(), rev_seq.end());
+            reverse(rev_qual.begin(), rev_qual.end());
+            compliment(rev_seq);
+
+            *fwd_stream << "@" << a.Name << endl << fwd_seq << endl << "+" << a.Name << endl << fwd_qual << endl;
+            *rev_stream << "@" << a.Name << endl << rev_seq << endl << "+" << a.Name << endl << rev_qual << endl;
+            
+            if(fwd_stream->fail() || rev_stream->fail()) {
+                cerr << "Error writing out line to fastq file. Aborting." << endl;
+                exit(-1);
+            }
+
+            potential_pairs.erase(it);
+        }
+    }
     
     return true;
 }
