60a0ea865b26fe4550ad5f93d3152dc2bdc6fe81
Lee Baker <lee@leecbaker.com>
2012-08-14 16:45:11 +0700

    Revert "Simplify AddTag code in BamAlignment."
    This reverts commit 22c849c38643015202488aab034a56625fb32240.

--- bamtools/src/api/BamAlignment.h
+++ bamtools/src/api/BamAlignment.h
@@ -216,11 +216,28 @@ inline bool BamAlignment::AddTag(const std::string& tag, const std::string& type
     // otherwise, convert value to string
     union { T value; char valueBuffer[sizeof(T)]; } un;
     un.value = value;
+#if 1
 
+    // copy original tag data to temp buffer
+    const std::string newTag = tag + type;
+    const size_t newTagDataLength = tagDataLength + newTag.size() + sizeof(T); // leave room for new T
+    RaiiBuffer originalTagData(newTagDataLength);
+    memcpy(originalTagData.Buffer, TagData.c_str(), tagDataLength + 1);    // '+1' for TagData null-term
+
+    // append newTag
+    strcat(originalTagData.Buffer + tagDataLength, newTag.data());
+    memcpy(originalTagData.Buffer + tagDataLength + newTag.size(), un.valueBuffer, sizeof(T));
+
+    // store temp buffer back in TagData
+    const char* newTagData = (const char*)originalTagData.Buffer;
+    TagData.assign(newTagData, newTagDataLength);
+    
+#else
+    // there has to be a good reason not to do this instead..
     TagData.append(tag);
     TagData.append(type);
     TagData.append(un.valueBuffer, sizeof(T));
-
+#endif
     return true;
 }
 
@@ -257,10 +274,24 @@ inline bool BamAlignment::AddTag<std::string>(const std::string& tag,
         return false;
     }
     
+#if 1   //switch in optimization to speed up tag data allocation
+    // otherwise, copy tag data to temp buffer
+    const std::string newTag = tag + type + value;
+    const size_t newTagDataLength = tagDataLength + newTag.size() + 1; // leave room for null-term
+    RaiiBuffer originalTagData(newTagDataLength);
+    memcpy(originalTagData.Buffer, TagData.c_str(), tagDataLength + 1);    // '+1' for TagData null-term
+
+    // append newTag (removes original null-term, then appends newTag + null-term)
+    strcat(originalTagData.Buffer + tagDataLength, newTag.data());
+
+    // store temp buffer back in TagData
+    const char* newTagData = (const char*)originalTagData.Buffer;
+    TagData.assign(newTagData, newTagDataLength);
+#else
     TagData.append(tag);
     TagData.append(type);
     TagData.append(value);
-
+#endif
     return true;
 }
 
