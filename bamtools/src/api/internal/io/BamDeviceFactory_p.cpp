7bcf2cd5f371298c1f6108ad6a3310c600bec0eb
Lee Baker <lee@leecbaker.com>
2012-08-27 17:12:02 +0700

    Pare down bamtools: Remove FTP/HTTP support from bamtools.

--- bamtools/src/api/internal/io/BamDeviceFactory_p.cpp
+++ bamtools/src/api/internal/io/BamDeviceFactory_p.cpp
@@ -9,8 +9,6 @@
 
 #include "api/internal/io/BamDeviceFactory_p.h"
 #include "api/internal/io/BamFile_p.h"
-#include "api/internal/io/BamFtp_p.h"
-#include "api/internal/io/BamHttp_p.h"
 #include "api/internal/io/BamPipe_p.h"
 using namespace BamTools;
 using namespace BamTools::Internal;
@@ -23,14 +21,6 @@ IBamIODevice* BamDeviceFactory::CreateDevice(const string& source) {
     // check for requested pipe
     if ( source == "-" || source == "stdin" || source == "stdout" )
         return new BamPipe;
-
-    // check for HTTP prefix
-    if ( source.find("http://") == 0 )
-        return new BamHttp(source);
-
-    // check for FTP prefix
-    if ( source.find("ftp://") == 0 )
-        return new BamFtp(source);
 
     // otherwise assume a "normal" file
     return new BamFile(source);
