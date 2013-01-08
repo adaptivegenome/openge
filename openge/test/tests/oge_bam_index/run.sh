3d68e15dd1fa36801b6f4945065bf4248d7d56d6
Lee Baker <lee@leecbaker.com>
2013-01-08 17:44:19 -0600

    Update indexing tests to compare with samtools output.

--- openge/test/tests/oge_bam_index/run.sh
+++ openge/test/tests/oge_bam_index/run.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 source $(dirname $0)/../common.sh
-rm -f test.bai oge.bam oge.bai oge2.bam oge2.bai
+rm -f test.bam.bai oge.bam oge.bam.bai oge2.bam oge2.bam.bai
 
 $OGE view $DATA/simple.bam -o oge1.bam
-[ ! -f oge2.bai ] && err "Failed to find oge1.bai"
-mv oge1.bai test.bai
+[ ! -f oge1.bam.bai ] && err "Failed to find oge1.bam.bai"
+mv oge1.bam.bai test.bai
 samtools index oge1.bam
 
 [ ! -f test.bai ] && err "Failed to find test.bai"
 
-cmp correct.bai oge.bai || err "BAM index differs (test 1)."
+cmp test.bai oge1.bam.bai || err "BAM index differs (test 1)."
 
-$OGE view $DATA/208.yhet.bam -o oge2.bam
-[ ! -f oge2.bai ] && err "Failed to find oge2.bai"
-mv oge2.bai test.bai
+#test with bigger, unsorted input
+$OGE mergesort $DATA/208.yhet.bam -o oge2.bam
+[ ! -f oge2.bam.bai ] && err "Failed to find oge2.bam.bai"
+mv oge2.bam.bai test.bai
 samtools index oge2.bam
 
 [ ! -f test.bai ] && err "Failed to find test.bai"
 
-cmp correct.bai oge.bai || err "BAM index differs (test 2)."
+cmp oge2.bam.bai test.bai || err "BAM index differs (test 2)."
 true
